cbfd8448a08ac51b94a06f5f23a0f6a1e2281aff
Mark Smertenko <125746052+markidzo@users.noreply.github.com>
2025-03-12 12:53:54 +0200

    Update mine portfolia

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-// alert("Welcome");
+document.getElementById('aboutMeLink').addEventListener('click', function(e) {
+    e.preventDefault(); // Запобігаємо стандартному переходу по якорю
+    const target = document.getElementById(this.getAttribute('href').substring(1));
+    
+    // Плавний скрол до елемента з id "abv"
+    window.scrollTo({
+        top: target.offsetTop,
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('projectLink').addEventListener('click', function(e) {
+    e.preventDefault(); // Запобігаємо стандартному переходу по якорю
+    const target = document.getElementById(this.getAttribute('href').substring(1));
+    
+    // Плавний скрол до елемента з id "abv"
+    window.scrollTo({
+        top: target.offsetTop,
+        behavior: 'smooth'
+    });
+});
+
+
+document.getElementById('skillLink').addEventListener('click', function(e) {
+    e.preventDefault(); // Запобігаємо стандартному переходу по якорю
+    const target = document.getElementById(this.getAttribute('href').substring(1));
+    
+    // Плавний скрол до елемента з id "abv"
+    window.scrollTo({
+        top: target.offsetTop,
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('contLink').addEventListener('click', function(e) {
+    e.preventDefault(); // Запобігаємо стандартному переходу по якорю
+    const target = document.getElementById(this.getAttribute('href').substring(1));
+    
+    // Плавний скрол до елемента з id "abv"
+    window.scrollTo({
+        top: target.offsetTop,
+        behavior: 'smooth'
+    });
+});
